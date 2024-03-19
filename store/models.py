c01d6b424f3d8fb277597da4f53c4e328ef9177b
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2024-03-19 20:02:18 +0100

    Still learning about models

--- store/models.py
+++ store/models.py
@@ -4,10 +4,6 @@ from django.db import models
 from django.db.models import PROTECT, CASCADE, SET_NULL
 
 
-# Create your models here.
-class Item(models.Model):
-    pass
-
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_products = models.ForeignKey(to='Product', on_delete=SET_NULL, related_name='+', null=True)
@@ -15,6 +11,7 @@ class Collection(models.Model):
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
+
 class Product(models.Model):
     title = models.CharField(max_length=255, null=False)
     description = models.TextField(null=False)
@@ -48,15 +45,26 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True, auto_created=True)
     payment_status = models.TextField(choices=PAYMENT_STATUS, max_length=1, default=Payment_Status.PENDING)
-    item = models.ForeignKey(to=Item, on_delete=PROTECT)
 
     def __repr__(self):
         return f"""
             {
-                'placed_at': {self.placed_at},
+        'placed_at': {self.placed_at},
                 payment_status: {self.payment_status}
             }
         """
+    # Create your models here.
+
+
+
+
+
+
+
+
+
+
+
 
 class Order_Item(models.Model):
     order = models.ForeignKey(to=Order, on_delete=PROTECT)
@@ -74,8 +82,8 @@ class Customer(models.Model):
         (Membership_Type.SILVER_MEMBERSHIP, 'Silver'),
         (Membership_Type.GOLD_MEMBERSHIP, 'Gold')
     ]
-    first_name = models.TextField(max_length=1000, null=False)
-    last_name = models.TextField(max_length=1000, null=False)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(error_messages="Invalid Email", unique=True)
     phone_number = models.TextField(max_length=11, null=False)
     profile_image = models.FileField()
@@ -109,8 +117,15 @@ class Address(models.Model):
     customer = models.OneToOneField(to=Customer, on_delete=CASCADE)
 
 class Cart(models.Model):
-    item = models.ForeignKey(to=Item, on_delete=PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Item(models.Model):
+    order_item = models.ForeignKey(to=Order, on_delete=PROTECT)
+    cart = models.ForeignKey(to=Cart, on_delete=PROTECT)
 
 class Cart_Item(models.Model):
-    pass
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
 
